d2c866568f928d309d70a9ed22a2ea06bb45c757
ShalomOdozi <shalomodozi7@gmail.com>
2020-09-16 12:59:00 +0100

    adding files

--- todo/settings.py
+++ todo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v_yc!+ez#d@gc=jkc!+2(z!%qb1$t-)_tauqu+4$x#@g8lh(+1'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
